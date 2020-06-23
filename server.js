2dc6089722e7c369ae0417d7eea47978b9979f74
Gary Llil <garyjllil@gmail.com>
2020-06-22 22:36:16 -0400

    added middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
 require("dotenv").config();
@@ -8,6 +9,7 @@ require("dotenv").config();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
